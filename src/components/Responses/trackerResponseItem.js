2a09987e45b656c25ef68c8bed5fcb6ecbb03570
Nico Jochnick <nico.jochnick@berkeley.edu>
2021-01-08 21:04:59 -0500

    figured out how to color draft js text the dumb way

--- src/components/Responses/trackerResponseItem.js
+++ src/components/Responses/trackerResponseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import Popover from "@material-ui/core/Popover/Popover";
@@ -6,19 +6,27 @@ import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Box from "@material-ui/core/Box";
 import Avatar from "@material-ui/core/Avatar";
-import {convertFromRaw, Editor, EditorState} from "draft-js";
+import {convertFromRaw, RichUtils, Editor, EditorState} from "draft-js";
 
 import {makeStyles} from "@material-ui/core/styles";
 import { FiMoreVertical } from "react-icons/fi";
 
 function TrackerResponseItem(props) {
 
-    console.log(props.response)
-    let editorState = null;
-    if (props.response.type !== 'numeric') {
-        editorState = EditorState.createWithContent(convertFromRaw(JSON.parse(props.response.response)));
+    const [eState, setEState] = React.useState(null)
 
-    }
+    console.log(props.response)
+
+
+    useEffect(() => {
+        let editorState = null;
+        if (props.response.type !== 'numeric') {
+            editorState = EditorState.createWithContent(convertFromRaw(JSON.parse(props.response.response)));
+            editorState = RichUtils.toggleInlineStyle(editorState, 'rgba(255, 0, 0, 1.0)',
+            );
+            setEState(editorState)
+        }
+    }, []);
 
 
     console.log(props.response);
@@ -64,8 +72,12 @@ function TrackerResponseItem(props) {
                                 </p>
 
 
-                                {(editorState !== null)
-                                    ? <Editor editorState={editorState} readOnly={true}/>
+                                {(eState!== null)
+
+                                    ?
+                                    <div style = {{color:'white', margin: 10, fontSize: 15}}>
+                                    <Editor customStyleMap={colorStyleMap} editorState={eState} readOnly={true}/>
+                                    </div>
                                     : null
                                 }
                                 </div>
@@ -80,6 +92,30 @@ function TrackerResponseItem(props) {
             }
         </div>
     );
+};
+
+const colorStyleMap = {
+    red: {
+        color: 'rgba(255, 0, 0, 1.0)',
+    },
+    orange: {
+        color: 'rgba(255, 127, 0, 1.0)',
+    },
+    yellow: {
+        color: 'rgba(180, 180, 0, 1.0)',
+    },
+    green: {
+        color: 'rgba(0, 180, 0, 1.0)',
+    },
+    blue: {
+        color: 'rgba(0, 0, 255, 1.0)',
+    },
+    indigo: {
+        color: 'rgba(75, 0, 130, 1.0)',
+    },
+    violet: {
+        color: 'rgba(127, 0, 255, 1.0)',
+    },
 };
 
 export default TrackerResponseItem;
