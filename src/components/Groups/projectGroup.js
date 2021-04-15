510a1a6ab1dd0e3ba88d0dcc55feb86554f146d9
Nico Jochnick <nico.jochnick@berkeley.edu>
2021-04-15 12:20:39 -0400

    add avatars on group to headeR

--- src/components/Groups/projectGroup.js
+++ src/components/Groups/projectGroup.js
@@ -54,13 +54,14 @@ export default function ProjectGroup(props) {
         <Box display='flex' flexDirection='row'>
             <Button aria-describedby={id} onClick={handleClick}>
                 {/*Open Popover*/}
-                <AvatarGroup max={4}>
-                    {}
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
-                    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg"/>
-                    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg"/>
-                    <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg"/>
-                    <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg"/>
+                <AvatarGroup max={8}>
+                    { group.map((item) => {
+                        return   <Box component="span" borderRadius = {100} border= {2} style ={{ padding: 1, margin: 5}} borderColor = {'#B9B9B9'} >
+                            <Avatar  src = {item.img_url_Profile.imgUrl}/>
+                        </Box>
+                    })
+
+                    }
                 </AvatarGroup>
             </Button>
             <Popover
