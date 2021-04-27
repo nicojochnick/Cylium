a2610ddf0dacc3ddaf182435d69dae3abc3eac00
Nico Jochnick <nico.jochnick@berkeley.edu>
2021-04-27 10:44:00 -0400

    switch with options, still needs formatting and functionality

--- src/components/Nodes/NodeUtils/nodeStylerBar.js
+++ src/components/Nodes/NodeUtils/nodeStylerBar.js
@@ -7,11 +7,30 @@ import Divider from "@material-ui/core/Divider";
 import { MdRoundedCorner } from "react-icons/md";
 import Tooltip from "@material-ui/core/Tooltip";
 import Popover from "@material-ui/core/Popover/Popover";
+import Slider from "@material-ui/core/Slider";
+import TextField from "@material-ui/core/TextField";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { CirclePicker } from 'react-color';
+
 
 
 function NodeStylerBar(props) {
 
     const [sliderOpen, setSliderOpen] = React.useState(false);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [option,setOption] = React.useState(null)
+
+    const handleOpenOption = (event, option) => {
+        setAnchorEl(event.currentTarget);
+        setOption(option)
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
 
     const onBackGroundColorChange = () =>{
 
@@ -22,14 +41,85 @@ function NodeStylerBar(props) {
 
     };
 
+    function valuetext(value) {
+        return `${value}`;
+    }
+
+    const renderSelect = (option ) => {
+
+        switch (option) {
+
+
+            case  'radius':
+                return <TextField
+                    id="standard-number"
+                    type="number"
+                    InputProps={{style: {fontSize: 15, margin: 5, color:'white'}, disableUnderline: true,}}
+                    InputLabelProps={{
+                        shrink: true,
+                        style : {color: 'white'}
+                    }}
+                />;
+            case 'shadow':
+                return <ButtonGroup size="small" >
+                    <Button>None</Button>
+                    <Button>Light</Button>
+                    <Button>Heavy</Button>
+                </ButtonGroup>;
+
+            case 'border':
+                return <ButtonGroup size="small" >
+                    <Button>0px</Button>
+                    <Button>1px</Button>
+                    <Button>3px</Button>
+                </ButtonGroup>;
+
+            case 'color':
+                return<Box display={'flex'} flexDirection = 'column'>
+                        <Button>Fill</Button>
+                        <CirclePicker/>
+                </Box>;
+            default:
+                return <div/>
+        }
+
+
+    };
+
 
     return (
 
         <Box display = ' flex' alignItems = 'center' justifyContent = 'center' border = {3} borderColor = {'white'}  style = {{backgroundColor: '#343434', height: 70, width: 450, boxShadow:`0px 3px 10px rgba(0, 0, 0, 0.15)`}} borderRadius = {10} flexDirection = 'row'>
+            <Popover
+                id={id}
+                style={
+                    {
+                        marginTop: -75,
+                    }
+                }
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
 
+                <Box style = {{minWidth: 80, backgroundColor: '#343434',minHeight: 30, padding: 4}} display = ' flex' alignItems = 'center' justifyContent = 'center'  flexDirection = 'row'>
+                    {renderSelect(option)}
+                </Box>
+
+
+
+            </Popover>
             <Tooltip title="Color" arrow placement={'top'} >
 
-            <Button>
+            <Button onClick = {(e)=>handleOpenOption(e,'color')}>
 
                 <Box border = {2} borderRadius = {30} borderColor = {'white'} style = {{height: 23, width: 23,padding: 2, backgroundColor: props.style.bgColor}}>
                 </Box>
@@ -38,7 +128,7 @@ function NodeStylerBar(props) {
 
             <Tooltip title="Border" arrow placement={'top'} >
 
-            <Button>
+            <Button onClick = {(e)=>handleOpenOption(e,'border')}>
                <BiFilter style = {{color: 'white'}} size = {30} />
             </Button>
 
@@ -46,7 +136,7 @@ function NodeStylerBar(props) {
 
             <Tooltip title="Shadow" arrow placement={'top'} >
 
-            <Button>
+            <Button onClick = {(e)=>handleOpenOption(e,'shadow')} >
                 <BiSquareRounded  style = {{color: 'white'}} size = {30}  />
 
             </Button>
@@ -55,7 +145,7 @@ function NodeStylerBar(props) {
 
 
             <Tooltip title="Radius" arrow placement={'top'} >
-                <Button onClick = {()=>setSliderOpen(true)}>
+                <Button onClick = {(e)=>handleOpenOption(e,'radius')}>
                 <MdRoundedCorner  style = {{color: 'white'}} size = {30}  />
                 </Button>
             </Tooltip>
@@ -63,7 +153,7 @@ function NodeStylerBar(props) {
 
             <Divider style = {{color:'white', backgroundColor:'white'}} orientation="vertical" flexItem />
 
-            <Tooltip title="Lockgit " arrow placement={'top'} >
+            <Tooltip title="Lock " arrow placement={'top'} >
 
             <Button>
                 <BiLock  style = {{color: 'white'}} size = {30}  />
