99b7b1d2c98e9ffb0b0c3588ed4c10e1e3a56ea8
Nico Jochnick <nico.jochnick@berkeley.edu>
2021-04-30 18:00:38 -0400

    node architecture files

---
+++ src/components/Nodes/NodeContainers/modalContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ModalContainer(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ModalContainer;
---
+++ src/components/Nodes/NodeTypes/dataNode.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DataNode(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default DataNode;
