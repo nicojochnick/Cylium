ed32674c8d95ceae5173eff9597540328b1d902f
Nico Jochnick <nico.jochnick@berkeley.edu>
2021-05-03 08:39:05 -0400

    temp styling on doc interface node

--- src/components/Nodes/NodeList/documentNode.js
+++ src/components/Nodes/NodeList/documentNode.js
@@ -6,6 +6,9 @@ import {Handle} from "react-flow-renderer";
 import NodeStylerBar from "../NodeUtils/nodeStylerBar";
 import Avatar from "@material-ui/core/Avatar";
 import NodeProfile from "../../Profile/Node/nodeProfile";
+import {BiDetail} from "react-icons/bi";
+import Divider from "@material-ui/core/Divider";
+import TextField from "@material-ui/core/TextField/TextField";
 
 export default memo(({ data,}) => {
     const [size, setSize] = React.useState(data.size);
@@ -27,38 +30,46 @@ export default memo(({ data,}) => {
         setBarOpen(false);
         setBarOpen(false)
     };
-
     const lock = () => {
         data.locked = !data.locked;
         setLocked(!locked)
     };
-
     const changeTitle = (text) => {
         data.title = text
     };
 
-
     return (
-        <>
-            <Box
-                style = {{marginLeft: 15}}
-                display='flex'
-                flexDirection='row'
-            >
-                <Box display = 'flex' flexDirection = 'row'   >
-                    <NodeProfile changeTitle = {changeTitle}  size = 'small'  type = {data.type} title = {data.title} />
-                </Box>
-                {/*{barOpen*/}
-                {/*    ?*/}
-                {/*    <div>*/}
-                {/*        {data.locked*/}
-                {/*            ? <BiLock onClick = {() => lock()} style={{margin: 10, color: 'grey '}} size={30}/>*/}
-                {/*            :  <BiLockOpenAlt onClick = {() => lock()} style={{margin: 10, color: 'grey '}} size={30}/>*/}
-                {/*        }*/}
-                {/*    </div>*/}
-                {/*    : null*/}
-                {/*}*/}
-            </Box>
-        </>
+        <Box display={'flex'} flexDirection ='row'  style={ {overflowX: 'hidden', padding: 5, margin:3, width: data.size[1], backgroundColor:'white'}}>
+            <TextField
+                onChange={(e)=> changeTitle(e.target.value)}
+                id="standard-basic"
+                placeholder="Untitled"
+                value={data.title}
+                InputProps={{style: {fontSize: 20, textTransform: 'capitalized', margin: 10, color:'#4B494D'}, disableUnderline: true,}}
+            />
+            {/*<Divider/>*/}
+            {/*<Box*/}
+            {/*    style = {{ height: data.size[1]/20, padding:2, width: data.size[0], overflow: 'hidden', backgroundColor:'white'}}*/}
+            {/*    display='flex'*/}
+            {/*    flexDirection='row'*/}
+            {/*>*/}
+
+            {/*</Box>*/}
+        </Box>
     );
 });
+
+
+{/*<Box display = 'flex' flexDirection = 'row'   >*/}
+{/*    <NodeProfile changeTitle = {changeTitle}  size = 'small'  type = {data.type} title = {data.title} />*/}
+{/*</Box>*/}
+{/*{barOpen*/}
+{/*    ?*/}
+{/*    <div>*/}
+{/*        {data.locked*/}
+{/*            ? <BiLock onClick = {() => lock()} style={{margin: 10, color: 'grey '}} size={30}/>*/}
+{/*            :  <BiLockOpenAlt onClick = {() => lock()} style={{margin: 10, color: 'grey '}} size={30}/>*/}
+{/*        }*/}
+{/*    </div>*/}
+{/*    : null*/}
+{/*}*/}
